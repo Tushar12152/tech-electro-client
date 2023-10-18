5c4cce10c30808c17feb5299d39211b262c96911
Tushar12152 <tusharimran470@gmail.com>
2023-10-17 22:52:27 -0600

    our brand section added

--- src/Components/Home/Footer/Footer.jsx
+++ src/Components/Home/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import { FaFacebook,FaInstagramSquare,FaTwitter } from 'react-icons/fa';
 
 const Footer = () => {
     return (
-        <footer className=" flex justify-around items-center p-4 text-neutral-content  bg-gradient-to-r from-green-500 to-cyan-500 ">
+        <footer className=" flex lg:flex-row  flex-col gap-6 mt-16  justify-around items-center p-4 text-neutral-content  bg-gradient-to-r from-green-500 to-cyan-500 ">
   <aside className="items-center ">
    <img className="w-[120px] rounded-full" src={" https://i.ibb.co/44f2rKh/360-F-102975288-L4aqjlx-Xo-Zrkg0-CVv-BXmn-S78ob-UY9-NFL.jpg"} alt="" />
    
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,13 @@
 import Banner from "./Banner/Banner";
+import Brands from "./Brands/Brands";
 import Footer from "./Footer/Footer";
 
 const Home = () => {
     return (
         <div>
              <Banner></Banner>
+             <h1 className="text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-green-500 to-cyan-500 text-center py-16">OUR BRANDS</h1>
+             <Brands></Brands>
              <Footer></Footer>
         </div>
     );
